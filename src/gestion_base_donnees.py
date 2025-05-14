a157a9c786cd0bb2585439edff531df9fba2734a
Lucas <lucas.bour@uha.fr>
2025-05-14 10:41:16 +0200

    Ajout nombre personne par salle

--- src/gestion_base_donnees.py
+++ src/gestion_base_donnees.py
@@ -41,7 +41,7 @@ def ajouter_utilisateur(nom : str, prenom : str, adresse_mail : str):
     donnees["utilisateurs"].append(nouveau_utilisateur)
     enregistrer_donnees(donnees)
 
-def ajouter_salle(nom : str, type : str):
+def ajouter_salle(nom : str, type : str, capacite : int):
     # Ajoute une nouvelle salle   
     donnees = charger_donnees() 
 
@@ -56,6 +56,7 @@ def ajouter_salle(nom : str, type : str):
     nouvelle_salle = {
         "nom": nom,
         "type": type
+        "capacite": capacite
     }    
 
     donnees["salles"].append(nouvelle_salle)
@@ -65,5 +66,5 @@ def ajouter_salle(nom : str, type : str):
 #ajouter_utilisateur("Ferry","Kevin","kevin.ferry@mail.com")
 #ajouter_utilisateur("Bour","Lucas","lucas.bour@mail.com")
 #print(charger_utilisateurs())
-#ajouter_salle("Salle 1","standard")
+ajouter_salle("Salle 1","standard", 10)
 #print(charger_donnees())
