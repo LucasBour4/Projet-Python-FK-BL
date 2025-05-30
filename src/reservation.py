8079b72dcd57dfa08ba4b57ab2b9178d7ebe51ef
FERRY <kevin.ferry@uha.fr>
2025-05-30 23:26:52 +0200

    Programme complet pour la réservation

--- src/reservation.py
+++ src/reservation.py
@@ -1,30 +1,21 @@
 from datetime import datetime
+from gestion_base_donnees import charger_donnees, enregistrer_donnees
+from interface_graphique import *
 
-
-class Room():
-    """
-    Room class to manage room details.
-    """
-    def __init__(self, room_name, capacity):
-        self.room_name = room_name
-        self.capacity = capacity
-
-    def __str__(self):
-        return f"Name: {self.room_name}, Capacity: {self.capacity}"
     
 class ReservationError(Exception):
-    """
-    Custom exception for reservation errors.
-    """
+    """ Custom exception for reservation errors."""
+
     def __init__(self, message):
         super().__init__(message)
         self.message = message
 
 class Reservation():
-    """
-    Reservation class to manage reservations.
-    """
+    """ Reservation class to manage reservations."""
+
     def __init__(self, reservation_id, customer_name, date, time):
+        """ Initialize a reservation with ID, customer name, date, and time. """
+
         self.reservation_id = reservation_id
         self.customer_name = customer_name
         self.date = date
@@ -34,13 +25,15 @@ class Reservation():
         self.members = set()
 
     def is_room_available(self, room : Room) -> bool:
-        if isinstance(room, Room) and room not in self.room:
+        """   Check if a room is available for reservation. """
+
+        if instance(room, Room) is False:
             raise ReservationError("La salle n'est pas disponible.")
+        return room not in self.room
 
     def is_valid(self):
-        """
-        Check if the reservation is valid.
-        """
+        """ Check if the reservation is valid."""
+
         try:
             datetime.strptime(self.date, '%Y-%m-%d')
             datetime.strptime(self.time, '%H:%M')
@@ -49,6 +42,8 @@ class Reservation():
             return False
         
     def borrow_room(self, room : Room, person):
+        """ Borrow a room for a member. """
+        
         if room not in self.room:
             raise ReservationError("La salle n'est pas disponible.")
         if person not in self.members:
@@ -58,5 +53,6 @@ class Reservation():
     
 
     def __str__(self):
+
         return (f"Reservation ID: {self.reservation_id}, Customer: {self.customer_name}, "
                 f"Date: {self.date}, Time: {self.time}, Rooms: {[str(room) for room in self.rooms]}")
